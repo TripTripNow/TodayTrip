2ea19b9cbc188846b8944367ecac8e8df1943061
youdame <112458620+youdame@users.noreply.github.com>
2024-01-19 16:47:13 +0900

    Chore : 리액트 쿼리 및 axios 초기 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,37 @@
-import '@/styles/globals.css';
+import { useState } from 'react';
 import type { AppProps } from 'next/app';
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import '@/styles/globals.css';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // Instead do this, which ensures each request has its own cache:
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </HydrationBoundary>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
