8c0f1b374adece7cc823ae107263172b9f3296f8
youdame <112458620+youdame@users.noreply.github.com>
2024-01-19 18:12:03 +0900

    Feat: axios instance 생성#2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,5 @@
 function Home() {
-  return (
-    <main>
-      <div className="text-xl">TailwindCSS</div>
-      <div
-        style={{
-          fontWeight: 800,
-          fontSize: '40px',
-          fontFamily: 'Pretendard',
-        }}
-      >
-        안녕 ?
-      </div>
-      <div
-        style={{
-          fontWeight: 700,
-          fontSize: '40px',
-          fontFamily: 'Pretendard',
-        }}
-      >
-        안녕 ?
-      </div>
-    </main>
-  );
+  return <main></main>;
 }
 
 export default Home;
