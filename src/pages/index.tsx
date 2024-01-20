a21dfc6549d1dbc168b8f93ed60063fe225601c8
전수빈 <soobin08011812@gmail.com>
2024-01-20 22:52:36 +0900

    Test: test 코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,4 @@
-import { useEffect } from 'react';
-
 function Home() {
-  function factorial(n) {
-    if (n === 0) {
-      return 1;
-    }
-    return n * factorial(n - 1);
-  }
-
-  useEffect(() => {
-    factorial(2);
-  }, []);
-
   return <main></main>;
 }
 
