cb2de1199d7a1afc5a9f975165e361ad5c7e926e
youdame <112458620+youdame@users.noreply.github.com>
2024-01-20 16:16:56 +0900

    fix: 중복되는 파일 제거 #2

--- src/apis/axiosInstance.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'https://sp-globalnomad-api.vercel.app/1-9/';
-
-const instance = axios.create({
-  baseURL: BASE_URL,
-});
-
-export default instance;
