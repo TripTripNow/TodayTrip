e1f0f73338ddc0e4121209b9e1f4abc034a32ad4
전수빈 <soobin08011812@gmail.com>
2024-01-20 22:08:14 +0900

    Test: test #3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from 'react';
+
 function Home() {
+  function factorial(n) {
+    if (n === 0) {
+      return 1;
+    }
+    return n * factorial(n - 1);
+  }
+
+  useEffect(() => {
+    factorial(2);
+  }, []);
+
   return <main></main>;
 }
 
