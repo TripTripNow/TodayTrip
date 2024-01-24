5766d7f52922c4d76564e2b6aeed0ab62523ff5a
Soeun Kim <summerkim0825@gmail.com>
2024-01-24 11:04:33 +0900

    Feat: date-picker 인풋 커스텀 #12

--- src/components/common/DatePicker/DatePicker.tsx
+++ src/components/common/DatePicker/DatePicker.tsx
@@ -1,15 +1,38 @@
-import { useState } from 'react';
-import ReactDatePicker from 'react-datepicker';
+import { forwardRef, useState } from 'react';
+import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import styles from './DatePickerInput.module.css';
+import CalendarIcon from '#/icons/icon-calendar.svg';
 
-function DatePicker() {
-  const [startDate, setStartDate] = useState(new Date());
+interface Props {
+  value?: Date | string;
+  onClick?: () => void;
+}
+
+function DatePickerInput() {
+  const [startDate, setStartDate] = useState<Date>(new Date());
+  const CustomInput = forwardRef<HTMLButtonElement | null>(({ value, onClick }: Props, ref) => (
+    <label className={styles.container}>
+      <button className={styles.input} onClick={onClick} ref={ref}>
+        {value !== undefined ? String(value) : ''}
+      </button>
+      <CalendarIcon className={styles.icon} />
+    </label>
+  ));
+
+  CustomInput.displayName = 'CustomInput';
+
   return (
     <div>
       <h3>Date Picker</h3>
-      <ReactDatePicker selected={startDate} onChange={(date: Date) => setStartDate(date)} />
+      <DatePicker
+        selected={startDate}
+        onChange={(date: Date) => setStartDate(date)}
+        dateFormat={'yy/MM/dd'}
+        customInput={<CustomInput />}
+      />
     </div>
   );
 }
 
-export default DatePicker;
+export default DatePickerInput;
