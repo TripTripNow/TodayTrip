e2923c1e5c8b724260c71d79bc233e6dbd609e33
전수빈 <soobin08011812@gmail.com>
2024-01-25 22:47:31 +0900

    Feat: mypage - 내정보 페이지 모바일 #28

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,10 +1,13 @@
 import MyPageLayout from '@/components/MyPage/MyPageLayout';
-import { ReactElement } from 'react';
+import { ChangeEvent, ReactElement, useCallback, useRef, useState } from 'react';
 import styles from './MyPage.module.css';
 import { FieldValues, useForm } from 'react-hook-form';
 import Input from '@/components/Input/Input';
 import { passwordCheck } from '@/utils/passwordCheck';
-
+import Image from 'next/image';
+import LogoImg from '#/images/img-kakao.png';
+import EditIcon from '#/icons/icon-edit.svg';
+import profileStyles from '@/components/MyPage/ProfileMenuBox.module.css';
 function MyPage() {
   const methods = useForm<FieldValues>({
     mode: 'onTouched',
@@ -27,14 +30,58 @@ function MyPage() {
 
     console.log(data);
   };
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+
+  const onUpload = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target && e.target.files) {
+      const targetFiles = e.target.files[0];
+      const selectedFiles = URL.createObjectURL(targetFiles);
+      setImageSrc(selectedFiles);
+      handlePostProfile(targetFiles);
+    }
+  };
+
+  const handlePostProfile = (imgUrl: File) => {
+    const imgFormData = new FormData();
+    imgFormData.append('image', imgUrl);
+    //TODO 이미지 url 생성 api 연동, imgFormData 넘겨주기
+  };
+
+  const handleUploadImg = useCallback(() => {
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.click();
+  }, []);
 
   return (
     <div className={styles.myPageContainer}>
-      <div className={styles.titleContainer}>
-        <div className={styles.title}>내 정보</div>
-        <button type="submit" form="mypageForm" disabled={!isValid}>
-          저장하기
-        </button>
+      <div className={styles.topContainer}>
+        <div className={styles.titleContainer}>
+          <div className={styles.title}>내 정보</div>
+          <button type="submit" form="mypageForm" disabled={!isValid}>
+            저장하기
+          </button>
+        </div>
+
+        <div className={profileStyles.profileContainer}>
+          <Image
+            src={imageSrc ? imageSrc : LogoImg}
+            className={profileStyles.profileImg}
+            alt="profileImg"
+            width={160}
+            height={160}
+          />
+          <EditIcon className={profileStyles.editIcon} onClick={handleUploadImg} />
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => onUpload(e)}
+            ref={inputRef}
+            className={profileStyles.imgInput}
+          />
+        </div>
       </div>
       <form onSubmit={handleSubmit(handleOnSubmit)} className={styles.formContainer} id="mypageForm">
         <Input
