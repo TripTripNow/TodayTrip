76091e54273377b5ffc89d4719ca41085bac3119
전수빈 <soobin08011812@gmail.com>
2024-01-25 22:31:13 +0900

    Feat: mypage 레이아웃 - 프로필 박스 이미지 업로드 추가 #28

--- src/components/MyPage/ProfileMenuBox.tsx
+++ src/components/MyPage/ProfileMenuBox.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ChangeEvent, useCallback, useRef, useState } from 'react';
 import styles from './ProfileMenuBox.module.css';
 import MypageIcon from '#/icons/icon-mypage.svg';
 import ReservationIcon from '#/icons/icon-reservation.svg';
@@ -43,17 +43,48 @@ const MENU_LIST = [
 function ProfileMenuBox() {
   const router = useRouter();
   const [isSelected, setIsSelected] = useState('내 정보');
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
 
   const handleMenuItem = (item: { title: string; link: string }) => {
     setIsSelected(item.title);
     router.push(item.link);
   };
 
+  const onUpload = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target && e.target.files) {
+      const targetFiles = e.target.files[0];
+      const selectedFiles = URL.createObjectURL(targetFiles);
+      setImageSrc(selectedFiles);
+      handlePostProfile(targetFiles);
+    }
+  };
+
+  const handlePostProfile = (imgUrl: File) => {
+    const imgFormData = new FormData();
+    imgFormData.append('image', imgUrl);
+    //TODO 이미지 url 생성 api 연동, imgFormData 넘겨주기
+  };
+
+  const handleUploadImg = useCallback(() => {
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.click();
+  }, []);
+
   return (
     <div className={styles.profileBoxContainer}>
       <div className={styles.profileContainer}>
-        <Image src={LogoImg} className={styles.profileImg} alt="profileImg" />
-        {isSelected === '내 정보' && <EditIcon className={styles.editIcon} />}
+        <Image
+          src={imageSrc ? imageSrc : LogoImg}
+          className={styles.profileImg}
+          alt="profileImg"
+          width={160}
+          height={160}
+        />
+        {isSelected === '내 정보' && <EditIcon className={styles.editIcon} onClick={handleUploadImg} />}
+        <input type="file" accept="image/*" onChange={(e) => onUpload(e)} ref={inputRef} className={styles.imgInput} />
       </div>
       <div className={styles.memuContainer}>
         {MENU_LIST.map((e, index) => {
