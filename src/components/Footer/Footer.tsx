ef72e9fb8c44bed74337b724c234931a624e910f
Soeun Kim <summerkim0825@gmail.com>
2024-01-26 20:47:17 +0900

    Refactor: 코드 리뷰 반영 #35

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 function Footer() {
   return (
     <div className={styles.container}>
-      <div className={styles.content}>©TodayTrip-2024</div>
+      <p className={styles.content}>©TodayTrip-2024</p>
     </div>
   );
 }
