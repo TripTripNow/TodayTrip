cc9b0722d0cdffc1e6be98f00b123456e21667b5
Soeun Kim <summerkim0825@gmail.com>
2024-01-26 18:46:36 +0900

    Refactor: 버튼 컴포넌트 리뷰 수정사항 반영 #12

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,21 +1,31 @@
-import { MouseEventHandler } from 'react';
-import styles from './Button.module.css';
 import clsx from 'clsx';
+import { MouseEventHandler, ReactNode } from 'react';
+import styles from './Button.module.css';
+
+type Color = 'green' | 'white';
+
+type ButtonType =
+  | 'default'
+  | 'auth'
+  | 'search'
+  | 'category'
+  | 'activity'
+  | 'reservation'
+  | 'modalSingle'
+  | 'modalDouble'
+  | 'time';
 
 export interface ButtonProps {
-  text: string;
+  children: ReactNode;
+  color: Color;
+  type: ButtonType;
   onClick?: MouseEventHandler<HTMLButtonElement>;
-  outline?: boolean;
-  inactive?: boolean;
 }
 
-function Button({ text, onClick, outline, inactive }: ButtonProps) {
+function Button({ children, onClick, color, type }: ButtonProps) {
   return (
-    <button
-      onClick={onClick}
-      className={clsx(styles.button, styles.common, outline && styles.outline, inactive && styles.inactive)}
-    >
-      {text}
+    <button onClick={onClick} className={clsx(styles.button, styles[type], styles[color])}>
+      {children}
     </button>
   );
 }
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,10 +1,6 @@
 import Dropdown from '@/components/DropDown/Dropdown';
-import AddActivityButton from '@/components/common/Button/AddActivityButton';
-import AuthButton from '@/components/common/Button/AuthButton';
-import ReserveButton from '@/components/common/Button/ReserveButton';
 import DatePickerInput from '@/components/common/DatePicker/DatePicker';
-import ModalButton from '@/components/common/Button/ModalButton';
-import CategoryButton from '@/components/common/Button/CategoryButton';
+import Button from '@/components/common/Button/Button';
 
 const lists = ['문화 예술', '식음료', '스포츠', '투어', '관광'];
 
@@ -13,19 +9,60 @@ function Test() {
     <div style={{ margin: 10, display: 'flex', flexDirection: 'column', gap: 5 }}>
       <DatePickerInput />
       <Dropdown lists={lists} />
-      <AddActivityButton text="등록하기" />
-      <AddActivityButton text="등록하기" inactive />
-      <AuthButton text="로그인" />
-      <AuthButton text="로그인" inactive />
-      <ReserveButton text="체험 등록" />
-      <ReserveButton text="예약취소" outline />
-      <ReserveButton text="예약불가" inactive />
-      <ModalButton text="확정하기" type="confirm" />
-      <ModalButton text="취소하기" type="confirm" outline />
-      <ModalButton text="14:00~15:00" type="time" />
-      <ModalButton text="14:00~15:00" type="time" outline />
-      <CategoryButton text="문화예술" />
-      <CategoryButton text="문화예술" outline />
+      <Button color="green" type="default">
+        저장하기
+      </Button>
+      <Button color="green" type="default">
+        체험 등록하기
+      </Button>
+      <Button color="green" type="auth">
+        로그인 하기
+      </Button>
+      <Button color="green" type="auth">
+        회원가입 하기
+      </Button>
+      <Button color="green" type="search">
+        검색하기
+      </Button>
+      <Button color="green" type="activity">
+        등록하기
+      </Button>
+      <Button color="green" type="activity">
+        수정하기
+      </Button>
+      <Button color="green" type="reservation">
+        후기 작성
+      </Button>
+      <Button color="white" type="reservation">
+        예약 취소
+      </Button>
+      <Button color="green" type="modalSingle">
+        예약하기
+      </Button>
+      <Button color="green" type="modalSingle">
+        확인
+      </Button>
+      <Button color="green" type="modalSingle">
+        작성하기
+      </Button>
+      <Button color="green" type="modalDouble">
+        확정하기
+      </Button>
+      <Button color="white" type="modalDouble">
+        거절하기
+      </Button>
+      <Button color="green" type="category">
+        문화·예술
+      </Button>
+      <Button color="white" type="category">
+        문화·예술
+      </Button>
+      <Button color="green" type="time">
+        14:00~15:00
+      </Button>
+      <Button color="white" type="time">
+        14:00~15:00
+      </Button>
     </div>
   );
 }
