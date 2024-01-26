446d1c694f2392a9a52b2504a17bcbafa32539c3
qooktree1 <qooktree@gmail.com>
2024-01-26 16:59:49 +0900

    Feat: 상세 카드 구현 #10

---
+++ src/components/Home/CardDetail/CardDetail.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+
+import styles from './CardDetail.module.css';
+import StarIcon from '#/icons/icon-star.svg';
+import { CardProps } from '@/components/Home/Card/Card';
+import { intToFloat } from '@/utils/intToFloat';
+import { priceFormat } from '@/utils/priceFormat';
+import Link from 'next/link';
+
+function CardDetail({ item }: { item: CardProps['item'] }) {
+  return (
+    <Link href={`/activities/:${item.id}`}>
+      <div className={styles.container}>
+        <div className={styles.imageWrapper}>
+          <Image src={item.bannerImageUrl} className={styles.image} fill sizes="100%" priority alt="체험 배너" />
+        </div>
+        <div className={styles.descriptionContainer}>
+          <div className={styles.descriptionTopWrapper}>
+            <div className={styles.reviewWrapper}>
+              <StarIcon />
+              <p>
+                {intToFloat(item.rating, 1)}
+                <span> ({item.reviewCount})</span>
+              </p>
+            </div>
+            <h3>{item.title}</h3>
+          </div>
+          <div className={styles.descriptionBottomWrapper}>
+            <em>₩ {priceFormat(item.price)}</em>
+            <p>/인</p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default CardDetail;
