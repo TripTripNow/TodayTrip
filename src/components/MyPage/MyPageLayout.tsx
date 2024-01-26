0d8cd49a31ae5f8cd7ae4b4344e7fc1a9e3a0bf9
전수빈 <soobin08011812@gmail.com>
2024-01-26 13:24:06 +0900

    Refactor: 불필요한 코드 삭제 #28

--- src/components/MyPage/MyPageLayout.tsx
+++ src/components/MyPage/MyPageLayout.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from 'react';
 import styles from './MyPageLayout.module.css';
 import ProfileMenuBox from '@/components/MyPage/ProfileMenuBox';
 import { FormProvider, useForm } from 'react-hook-form';
-import { useRouter } from 'next/router';
 
 interface MyPageLayoutProps {
   children: ReactNode;
--- src/pages/mypage/reservations/index.tsx
+++ src/pages/mypage/reservations/index.tsx
@@ -1,13 +1,13 @@
 import MyPageLayout from '@/components/MyPage/MyPageLayout';
 import { ReactElement } from 'react';
 
-const Reservations = () => {
+function Reservations() {
   return (
     <div>
       <div>예약 내역</div>
     </div>
   );
-};
+}
 
 export default Reservations;
 Reservations.getLayout = (page: ReactElement) => <MyPageLayout>{page}</MyPageLayout>;
