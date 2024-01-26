7db4b7179eb92317d58887f8df8466d0882ec043
youdame <112458620+youdame@users.noreply.github.com>
2024-01-26 18:09:26 +0900

    Feat : 리뷰 모달 퍼블리싱 및 별점 컴포넌트 #26

--- src/components/Modal/ReviewModal/ReviewModal.tsx
+++ src/components/Modal/ReviewModal/ReviewModal.tsx
@@ -1,14 +1,43 @@
 import ModalLayout from '@/components/Modal/ModalLayout/ModalLayout';
 import styles from './ReviewModal.module.css';
 import CloseIcon from '#/icons/icon-close.svg';
+import clsx from 'clsx';
+import { reservations } from '@/pages/mypage/reservations/mock';
+import formatDateString from '@/utils/formatDateString';
+import Image from 'next/image';
+
 function ReviewModal() {
+  const data = reservations[4];
+
   return (
     <ModalLayout>
-      <div className={styles.wrapper}>
+      <div className={styles.modalWrapper}>
         <div className={styles.header}>
           <h2 className={styles.h2}>후기 작성</h2>
           <CloseIcon />
         </div>
+        <div className={styles.cardWrapper}>
+          <div className={styles.imageWrapper}>
+            <Image fill src={data.activity.bannerImageUrl} alt="체험 이미지" />
+          </div>
+
+          <div className={styles.detailContainer}>
+            <h2 className={styles.title}>{data.activity.title}</h2>
+            <div className={styles.dateDetail}>
+              <span>{formatDateString(data.date)}</span>
+              <div>·</div>
+              <span>
+                {data.startTime} - {data.endTime}
+              </span>
+              <div>·</div>
+              <span>{data.headCount}명</span>
+            </div>
+            <div className={styles.separator}></div>
+            <span className={styles.price}>₩{data.totalPrice.toLocaleString('ko-KR')}</span>
+          </div>
+        </div>
+
+        <div></div>
       </div>
     </ModalLayout>
   );
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,19 +1,29 @@
-import CancelModal from '@/components/Modal/CancelModal/CancelModal';
-import ReviewModal from '@/components/Modal/ReviewModal/ReviewModal';
+// Test.js
+
 import { useState } from 'react';
+import Rating from '@/components/Modal/ReviewModal/Rating/Rating';
 
 function Test() {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleModalToggle = () => {
-    setIsOpen((prev) => !prev);
+  // 상위 컴포넌트에서 관리할 상태인 ratingValue를 useState를 통해 생성하고 초기값을 0으로 설정합니다.
+  const [ratingValue, setRatingValue] = useState(0);
+
+  // ratingValue를 변경하는 handleChange 함수를 선언합니다.
+  const handleChange = (value) => {
+    setRatingValue(value); // setRatingValue를 호출하여 ratingValue의 값을 변경합니다.
   };
 
+  // Rating 컴포넌트를 렌더링하고, 필요한 props를 전달합니다.
   return (
-    <>
-      <ReviewModal />
-      <button onClick={handleModalToggle}>열기</button>
-      {isOpen && <CancelModal handleModalClose={handleModalToggle} handleCancel={handleModalToggle} />}
-    </>
+    <div style={{ display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+      <Rating
+        className="RatingInput"
+        value={ratingValue} // 상위 컴포넌트에서 관리하는 ratingValue 상태를 전달합니다.
+        onSelect={handleChange} // 상위 컴포넌트에서 정의한 handleChange 함수를 onSelect props로 전달합니다.
+        onHover={setRatingValue} // ratingValue 상태를 변경하는 setRatingValue 함수를 onHover props로 전달합니다.
+        onMouseOut={setRatingValue} // ratingValue 상태를 변경하는 setRatingValue 함수를 onMouseOut props로 전달합니다.
+      />
+    </div>
   );
 }
+
 export default Test;
