a0613e86aa5a774d212195c77a8ebd6e0c712c62
Soeun Kim <summerkim0825@gmail.com>
2024-01-27 20:33:03 +0900

    Style: 불필요한 코드 제거 #32

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -5,8 +5,7 @@ function Test() {
   return (
     <div>
       <FilterDropDown type="가격" lists={PRICE_LIST} />
-      {/* <FilterDropDown type="가격" lists={PRICE_LIST} /> */}
-      {/* <FilterDropDown type="예약 상태" lists={FILTER_LIST} /> */}
+      <FilterDropDown type="예약 상태" lists={FILTER_LIST} />
     </div>
   );
 }
