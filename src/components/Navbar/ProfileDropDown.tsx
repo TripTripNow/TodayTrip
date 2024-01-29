c6a19b439892c1b8454414e46cff3299b34430c6
박경서 <zerm0907@gmail.com>
2024-01-29 10:12:20 +0900

    Fix: 안쓰이는 부분 삭제 #13

--- src/components/Navbar/ProfileDropDown.tsx
+++ src/components/Navbar/ProfileDropDown.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import styles from './ProfileDropDown.module.css';
-import { useEffect } from 'react';
 
 const DropDown_ARR = [
   { link: '/mypage', title: '내 정보' },
@@ -14,22 +13,19 @@ interface ProfileDropDownProps {
 }
 
 function ProfileDropDown({ isOpen }: ProfileDropDownProps) {
-  useEffect(() => {
-    console.log(isOpen), [isOpen];
-  });
   return (
     <div className={isOpen ? styles.profileDropDownContainer : styles.profileDropDownCloseContainer}>
       {DropDown_ARR.map((item, index) => (
         <div className={styles.profileDropDownContent} key={index}>
-          <Link href={item.link}>
-            <p className={styles.profileDropDown}>{item.title}</p>
+          <Link href={item.link} className={styles.profileDropDown}>
+            {item.title}
           </Link>
         </div>
       ))}
       <hr className={styles.styledHr} />
       <div className={styles.profileDropDownContent}>
-        <Link href="/">
-          <p className={styles.profileDropDown}>로그아웃</p>
+        <Link href="/" className={styles.profileDropDown}>
+          로그아웃
         </Link>
       </div>
     </div>
