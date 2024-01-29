227bf84593fab7607a016b7f5fff1863a2c05800
Soeun Kim <summerkim0825@gmail.com>
2024-01-29 22:34:37 +0900

    Chore: test 코드 삭제 #32

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,11 +1,4 @@
-import FilterDropDown from '@/components/FilterDropdown/FilterDropdown';
-
 function Test() {
-  return (
-    <>
-      <FilterDropDown type="가격" />
-      <FilterDropDown type="예약 상태" />
-    </>
-  );
+  return <></>;
 }
 export default Test;
