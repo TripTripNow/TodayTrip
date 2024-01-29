41dcb388cc5e20f13ea61b9817f999b23828ff2c
qooktree1 <qooktree@gmail.com>
2024-01-29 21:17:38 +0900

    Fix: push 오류 수정 #46

--- src/pages/mypage/activities/reservation-dashboard/index.tsx
+++ src/pages/mypage/activities/reservation-dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 
 import MyPageLayout from '@/components/MyPage/MyPageLayout';
 
@@ -6,10 +6,12 @@ import styles from './ReservationDashboard.module.css';
 import Dropdown from '@/components/common/DropDown/Dropdown';
 
 function ReservationDashboard() {
+  const [dropdownItem, setDropdownItem] = useState<string | number>(''); // 드랍다운 value 값
+
   return (
     <div>
       <h1 className={styles.title}>예약 현황</h1>
-      <Dropdown />
+      <Dropdown setDropdownItem={setDropdownItem} />
       <div></div>
     </div>
   );
