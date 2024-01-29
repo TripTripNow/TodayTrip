92cf7422084cc3b9c8cbe1c257b48909b4213f4e
qooktree1 <qooktree@gmail.com>
2024-01-29 21:12:30 +0900

    Fix: 카테고리 드랍다운 예약 현황도 props로 받을 수 있게 수정 #46

--- src/components/common/DropDown/Dropdown.tsx
+++ src/components/common/DropDown/Dropdown.tsx
@@ -1,23 +1,52 @@
+import { MouseEvent, useState } from 'react';
+import clsx from 'clsx';
+
 import ArrowDownIcon from '#/icons/icon-arrowdown.svg';
 import ArrowUpIcon from '#/icons/icon-arrowup.svg';
-// import DropdownMenu from '@/components/DropDown/DropdownMenu';
 import CheckIcon from '#/icons/icon-checkmark.svg';
-import clsx from 'clsx';
-import { useState } from 'react';
+
 import styles from './Dropdown.module.css';
 
-const lists = ['문화 예술', '식음료', '스포츠', '투어', '관광', '웰빙'];
+const CATEGORY_LIST = ['문화 예술', '식음료', '스포츠', '투어', '관광', '웰빙'] as const;
 
-function Dropdown() {
+interface DropdownProps {
+  activities?: {
+    id: number;
+    userId: number;
+    title: string;
+    description: string;
+    category: string;
+    price: number;
+    address: string;
+    bannerImageUrl: string;
+    rating: number;
+    reviewCount: number;
+    createdAt: string;
+    updatedAt: string;
+  }[];
+  setDropdownItem: (val: string | number) => void;
+}
+
+function Dropdown({ activities, setDropdownItem }: DropdownProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const [value, setValue] = useState('카테고리');
+  const [value, setValue] = useState(activities ? activities[0].title : '카테고리');
 
   const handleDropdownToggle = () => {
     setIsOpen((prev) => !prev);
   };
 
+  const handleDropdownClick = (e: MouseEvent<HTMLDivElement>, val: string) => {
+    e.preventDefault();
+    setValue(val);
+    setDropdownItem(val);
+    handleDropdownToggle();
+  };
+
+  const showLists = activities ? activities.map((activity) => activity.title) : CATEGORY_LIST;
+
   return (
-    <div className={styles.container} style={{ border: '1px solid #000' }}>
+    <div className={styles.container} style={{ border: '1px solid #000' }} onBlur={handleDropdownToggle}>
+      {activities && <p className={styles.subTitle}>체험명</p>}
       <button
         value={value}
         className={clsx(styles.wrapper, value === '카테고리' && styles.placeholder)}
@@ -28,15 +57,8 @@ function Dropdown() {
       </button>
       {isOpen && (
         <div className={styles.menu}>
-          {lists.map((value, index) => (
-            <div
-              key={index}
-              className={styles.list}
-              onClick={() => {
-                setValue(value);
-                setIsOpen(!isOpen);
-              }}
-            >
+          {showLists.map((value, index) => (
+            <div key={index} className={styles.list} onMouseDown={(e) => handleDropdownClick(e, value)}>
               <CheckIcon className={styles.icon} />
               {value}
             </div>
