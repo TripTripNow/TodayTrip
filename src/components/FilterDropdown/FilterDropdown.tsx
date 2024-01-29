a4b93a28789e8bc7b785acbc3859f576d251ba38
Soeun Kim <summerkim0825@gmail.com>
2024-01-29 18:16:16 +0900

    Refactor: 필터 드롭다운 CSS #32

--- src/components/FilterDropdown/FilterDropdown.tsx
+++ src/components/FilterDropdown/FilterDropdown.tsx
@@ -1,7 +1,8 @@
 import ArrowDownIcon from '#/icons/icon-arrowDown-solid.svg';
 import { PRICE_LIST, PriceSortOption, RESERVE_LIST, ReserveSortOption } from '@/constants/dropdown';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './FilterDropdown.module.css';
+import clsx from 'clsx';
 
 interface FilterDropdownProps {
   type: '가격' | '예약 상태';
@@ -13,8 +14,10 @@ type AllReserveOption = ReserveSortOption | '예약 상태';
 function FilterDropDown({ type }: FilterDropdownProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [value, setValue] = useState<AllPriceOption | AllReserveOption>(type);
+  const [isMobile, setIsMobile] = useState(false);
+  const [windowSize, setWindowSize] = useState(typeof window !== 'undefined' ? window.innerWidth : 0);
 
-  const lists = type === '가격' ? PRICE_LIST : RESERVE_LIST;
+  const lists = type === '가격' ? (isMobile ? ['낮은 순', '높은 순'] : PRICE_LIST) : RESERVE_LIST;
 
   const handleDropdown = () => {
     setIsOpen((prev) => !prev);
@@ -23,11 +26,33 @@ function FilterDropDown({ type }: FilterDropdownProps) {
   const handleClickOutside = () => {
     setTimeout(() => {
       setIsOpen(false);
-    }, 200);
+    }, 150);
   };
 
+  const handleResize = () => {
+    setWindowSize(window.innerWidth);
+  };
+
+  const handleMobileSizeUpdate = () => {
+    if (windowSize < 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    handleMobileSizeUpdate();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      handleMobileSizeUpdate();
+    };
+  }, [windowSize, isMobile]);
+
   return (
-    <div className={styles.container} onBlur={handleClickOutside}>
+    <div onBlur={handleClickOutside}>
       <label className={styles.wrapper}>
         <button value={value} onClick={handleDropdown}>
           {value}
@@ -35,7 +60,7 @@ function FilterDropDown({ type }: FilterDropdownProps) {
         {value === type ? <ArrowDownIcon /> : <ArrowDownIcon style={{ rotate: '180deg' }} />}
       </label>
       {isOpen && (
-        <div className={styles.menu} onClick={handleDropdown}>
+        <div className={styles.option} onClick={handleDropdown}>
           {lists.map((item, idx) => (
             <button key={idx} className={styles.list} onClick={() => setValue(item)}>
               {item}
