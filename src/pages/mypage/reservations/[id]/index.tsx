1ac1b6bad92d873164de59b461e9311241957d5b
전수빈 <soobin08011812@gmail.com>
2024-01-30 17:37:57 +0900

    Delete: test 파일 삭제 #51

--- src/pages/mypage/reservations/[id]/index.tsx
+++
@@ -1,9 +0,0 @@
-import MyPageLayout from '@/components/MyPage/MyPageLayout';
-import { ReactElement } from 'react';
-
-const Id = () => {
-  return <div>a</div>;
-};
-export default Id;
-
-Id.getLayout = (page: ReactElement) => <MyPageLayout>{page}</MyPageLayout>;
