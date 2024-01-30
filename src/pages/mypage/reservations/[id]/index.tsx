dae8b97e89ed2cf947f0442ee2f863f4aca8efb7
Soeun Kim <summerkim0825@gmail.com>
2024-01-30 23:08:59 +0900

    Refactor: 반응형 CSS 수정 #56

--- src/pages/mypage/reservations/[id]/index.tsx
+++ src/pages/mypage/reservations/[id]/index.tsx
@@ -2,7 +2,7 @@ import ArrowIcon from '#/icons/icon-arrowBack.svg';
 import ExampleImg from '#/images/img-dance.jpg';
 import MyPageLayout from '@/components/MyPage/MyPageLayout';
 import Button from '@/components/common/Button/Button';
-import styles from './Reservationid.module.css';
+import styles from './ReservationId.module.css';
 
 import Image from 'next/image';
 import { ReactElement } from 'react';
@@ -11,8 +11,8 @@ function ReservationID() {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <ArrowIcon className={styles.backIcon} />
-        <div className={styles.exit}>뒤로 가기</div>
+        <ArrowIcon className={styles.iconBack} alt="뒤로 가기 아이콘" />
+        <div className={styles.back}>뒤로 가기</div>
       </div>
       <div className={styles.main}>
         <h1 className={styles.h1}>예약 상세</h1>
@@ -20,15 +20,13 @@ function ReservationID() {
           <Image priority fill src={ExampleImg} alt="예약 이미지" />
         </div>
 
-        <div className={styles.contents}>
+        <div className={styles.content}>
           <div className={styles.status}>
             <div className={styles.pending}>예약 신청</div>
             <div className={styles.confirmed}>예약 승인</div>
             <div className={styles.completed}>체험 완료</div>
           </div>
-          <h2 className={styles.title}>
-            함께 배우면 즐거릿 면함께 배우면 즐거운 스트릿 댄스 배우면 즐거운 스트릿 댄스
-          </h2>
+          <h2 className={styles.title}>함께 배우면 즐거릿 면함께 배우면 면면면면면</h2>
           <p className={styles.date}>
             <span>2023. 2. 14</span>
             <span> · </span>
@@ -39,7 +37,7 @@ function ReservationID() {
           <p>스트릿 댄스 상세 주소 추가</p>
         </div>
 
-        <div className={styles.footer}>
+        <div className={styles.bottom}>
           <div className={styles.price}>￦10,000</div>
           <Button type="reservation" color="green">
             예약 취소
