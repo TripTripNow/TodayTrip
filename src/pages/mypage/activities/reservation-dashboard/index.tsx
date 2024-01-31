8d7a8980acb20af0bf95bc36ed961b17246480c2
zermzerm <zerm0907@gmail.com>
2024-01-31 22:11:51 +0900

    Fix: 빌드 오류 수정 #45

--- src/pages/mypage/activities/reservation-dashboard/index.tsx
+++ src/pages/mypage/activities/reservation-dashboard/index.tsx
@@ -6,12 +6,12 @@ import styles from './ReservationDashboard.module.css';
 import Dropdown from '@/components/common/DropDown/Dropdown';
 
 function ReservationDashboard() {
-  const [dropdownItem, setDropdownItem] = useState<string | number>(''); // 드랍다운 value 값
+  const [dropdownItem, setDropdownItem] = useState<string>(''); // 드랍다운 value 값
 
   return (
     <div>
       <h1 className={styles.title}>예약 현황</h1>
-      <Dropdown setDropdownItem={setDropdownItem} />
+      <Dropdown setDropdownItem={setDropdownItem} type="예약한 시간" />
       <div></div>
     </div>
   );
