cdded47d16f8d7cdce1cbc30958c12fad6670773
qooktree1 <qooktree@gmail.com>
2024-02-01 12:29:08 +0900

    Fix: 드랍다운 컴포넌트 수정 #47

--- src/components/common/DropDown/Dropdown.tsx
+++ src/components/common/DropDown/Dropdown.tsx
@@ -1,35 +1,26 @@
 import { Dispatch, MouseEvent, SetStateAction, useState } from 'react';
 import clsx from 'clsx';
 
+import { CATEGORY_LIST } from '@/constants/dropdown';
 import ArrowDownIcon from '#/icons/icon-arrowdown.svg';
 import ArrowUpIcon from '#/icons/icon-arrowup.svg';
 import CheckIcon from '#/icons/icon-checkmark.svg';
-
 import styles from './Dropdown.module.css';
 
-const CATEGORY_LIST = ['문화 예술', '식음료', '스포츠', '투어', '관광', '웰빙'] as const;
-
 interface DropdownProps {
-  activities?: {
-    id: number;
-    userId: number;
-    title: string;
-    description: string;
-    category: string;
-    price: number;
-    address: string;
-    bannerImageUrl: string;
-    rating: number;
-    reviewCount: number;
-    createdAt: string;
-    updatedAt: string;
-  }[];
-  setDropdownItem: Dispatch<SetStateAction<string | number>>;
+  type: '시간' | '카테고리' | '예약한 시간' | '체험';
+  items?: string[];
+  setDropdownItem: Dispatch<SetStateAction<string>>;
 }
 
-function Dropdown({ activities, setDropdownItem }: DropdownProps) {
+const dropdownInitialValue = (type: string, items: string[]) => {
+  if (type === '카테고리') return '카테고리';
+  else return items[0];
+};
+
+function Dropdown({ items, setDropdownItem, type }: DropdownProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const [value, setValue] = useState(activities ? activities[0].title : '카테고리');
+  const [value, setValue] = useState(dropdownInitialValue(type, items ?? []));
 
   const handleDropdownToggle = () => {
     setIsOpen((prev) => !prev);
@@ -46,11 +37,11 @@ function Dropdown({ activities, setDropdownItem }: DropdownProps) {
     handleDropdownClose();
   };
 
-  const showLists = activities ? activities.map((activity) => activity.title) : CATEGORY_LIST;
+  const showLists = items ? items.map((item) => item) : CATEGORY_LIST;
 
   return (
-    <div className={styles.container} style={{ border: '1px solid #000' }} onBlur={handleDropdownClose}>
-      {activities && <p className={styles.subTitle}>체험명</p>}
+    <div className={clsx(styles.container, type === '시간' && styles.timeContainer)} onBlur={handleDropdownClose}>
+      {type === '체험' && <p className={styles.subTitle}>체험명</p>}
       <button
         value={value}
         className={clsx(styles.wrapper, value === '카테고리' && styles.placeholder)}
