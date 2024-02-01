d02fa7e29aaeeab0cf91cc30f1bd5cb5bc4c0df7
qooktree1 <qooktree@gmail.com>
2024-02-01 12:28:14 +0900

    Feat: 예약 현황페이지 작성  #47

--- src/pages/mypage/activities/reservation-dashboard/index.tsx
+++ src/pages/mypage/activities/reservation-dashboard/index.tsx
@@ -1,19 +1,31 @@
 import { ReactElement, useState } from 'react';
 
 import MyPageLayout from '@/components/MyPage/MyPageLayout';
+import Dropdown from '@/components/common/DropDown/Dropdown';
+import Calendar from '@/components/ReservationDashboard/Calendar/Calendar';
 
 import styles from './ReservationDashboard.module.css';
-import Dropdown from '@/components/common/DropDown/Dropdown';
+import { RESERVATION_DETAILS_MONTH_MOCK_DATA, RESERVATION_TITLE_MOCK_DATA } from '@/constants/mock';
+import NoResult from '@/components/Home/NoResult/NoResult';
 
 function ReservationDashboard() {
-  const [dropdownItem, setDropdownItem] = useState<string | number>(''); // 드랍다운 value 값
+  const [dropdownItem, setDropdownItem] = useState<string>(''); // 드랍다운 value 값
+  const experiencesData = RESERVATION_TITLE_MOCK_DATA?.activities.map((activity) => activity.title) ?? [];
+
+  const calendarItems = RESERVATION_DETAILS_MONTH_MOCK_DATA;
 
   return (
-    <div>
+    <>
       <h1 className={styles.title}>예약 현황</h1>
-      <Dropdown setDropdownItem={setDropdownItem} />
-      <div></div>
-    </div>
+      {experiencesData.length > 0 ? (
+        <>
+          <Dropdown type="체험" items={experiencesData} setDropdownItem={setDropdownItem} />
+          <Calendar items={calendarItems} />
+        </>
+      ) : (
+        <NoResult text="아직 등록한 체험이 없어요" />
+      )}
+    </>
   );
 }
 
