2bc5f608bc1d40ab17f73b8f935e2e4504590739
youdame <112458620+youdame@users.noreply.github.com>
2024-02-01 15:07:14 +0900

    Feat : 달력 라이브러리 삽입 #53

--- src/pages/activities/[id]/index.tsx
+++ src/pages/activities/[id]/index.tsx
@@ -1,5 +1,17 @@
 import Header from '@/components/Activities/Header';
 import styles from './Activity.module.css';
+import { useEffect, useState } from 'react';
+import { GoogleMap, useJsApiLoader, MarkerF, InfoWindowF } from '@react-google-maps/api';
+import axios from 'axios';
+import LocationIcon from '#/icons/icon-location.svg';
+
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 interface SubImageUrl {
   id: number;
   imageUrl: string;
@@ -22,6 +34,11 @@ export interface ActivityProps {
 }
 
 function Activity() {
+  const containerStyle = {
+    width: '100%',
+    height: '43em',
+  };
+
   const data = {
     id: 7,
     userId: 21,
@@ -30,7 +47,7 @@ function Activity() {
       '안녕하세요! 저희 스트릿 댄스 체험을 소개합니다. 저희는 신나고 재미있는 스트릿 댄스 스타일을 가르칩니다. 크럼프는 세계적으로 인기 있는 댄스 스타일로, 어디서든 춤출 수 있습니다. 저희 체험에서는 새로운 스타일을 접할 수 있고, 즐거운 시간을 보낼 수 있습니다. 저희는 초보자부터 전문가까지 어떤 수준의 춤추는 사람도 가르칠 수 있도록 준비해놓았습니다. 저희와 함께 즐길 수 있는 시간을 기대해주세요! 각종 음악에 적합한 스타일로, 저희는 크럼프 외에도 전통적인 스트릿 댄스 스타일과 최신 스트릿 댄스 스타일까지 가르칠 수 있습니다. 저희 체험에서는 전문가가 직접 강사로 참여하기 때문에, 저희가 제공하는 코스는 어떤 수준의 춤추는 사람도 쉽게 이해할 수 있도록 준비해놓았습니다. 저희 체험을 참가하게 된다면, 즐거운 시간 뿐만 아니라 새로운 스타일을 접할 수 있을 것입니다.',
     category: '투어',
     price: 10000,
-    address: '서울특별시 강남구 테헤란로 427',
+    address: '서울 중구 삼일대로 343 대신파이낸스센터 8층',
     bannerImageUrl: '/images/a.png',
     subImageUrls: [
       {
@@ -55,6 +72,31 @@ function Activity() {
     createdAt: '2023-12-31T21:28:50.589Z',
     updatedAt: '2023-12-31T21:28:50.589Z',
   };
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY as string,
+  });
+
+  const [location, setLocation] = useState<{ lat: number; lng: number }>();
+  const [value, onChange] = useState<Value>(new Date());
+
+  useEffect(() => {
+    const fetchLocation = async () => {
+      try {
+        const response = await axios.get('/api/geocode', {
+          params: {
+            address: data.address,
+          },
+        });
+
+        const { lat, lng } = response.data;
+        setLocation({ lng, lat });
+      } catch (error) {
+        console.error('Error fetching location:', error);
+      }
+    };
+    fetchLocation();
+  }, []);
 
   return (
     <div className={styles.wrapper}>
@@ -71,9 +113,42 @@ function Activity() {
                 </div>
               </div>
               <hr className={styles.hr} />
+              {isLoaded && location && (
+                <GoogleMap
+                  mapContainerStyle={containerStyle}
+                  zoom={18}
+                  center={{ lat: location.lat, lng: location.lng }}
+                >
+                  <MarkerF
+                    position={location}
+                    icon={{ url: '/images/img-mapMaker.png', scaledSize: new window.google.maps.Size(60, 70) }}
+                  />
+                  <InfoWindowF
+                    options={{ pixelOffset: new window.google.maps.Size(0, -40) }}
+                    position={{ lat: location.lat, lng: location.lng }}
+                    zIndex={1}
+                  >
+                    <div>{data.address}</div>
+                  </InfoWindowF>
+                </GoogleMap>
+              )}
+              <p className={styles.location}>
+                <LocationIcon alt="지도 마커 아이콘" />
+                {data.address}
+              </p>
+            </div>
+            <div className={styles.rightContentContainer}>
+              <div>
+                <Calendar
+                  calendarType="gregory"
+                  locale="en"
+                  onChange={onChange}
+                  className={styles.customCalendar}
+                  value={value}
+                />
+              </div>
             </div>
           </section>
-          <div className={styles.reserve}></div>
         </div>
       </main>
     </div>
--- src/pages/test/test2.tsx
+++ src/pages/test/test2.tsx
@@ -1,53 +1,17 @@
-import { useEffect, useState } from 'react';
-import { GoogleMap, useJsApiLoader, MarkerF, InfoWindowF } from '@react-google-maps/api';
-import axios from 'axios';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+type ValuePiece = Date | null;
 
-const containerStyle = {
-  width: '100%',
-  height: '400px',
-};
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-const myStyles = [
-  {
-    featureType: 'poi',
-    elementType: 'labels',
-    // stylers: [{ visibility: 'off' }],
-  },
-];
 function Test2() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY as string,
-  });
+  const [value, onChange] = useState<Value>(new Date());
 
-  const [location, setLocation] = useState<{ lat: number; lng: number }>();
-
-  useEffect(() => {
-    const fetchLocation = async () => {
-      try {
-        const response = await axios.get('/api/location', {
-          params: {
-            address: 'EPITA - School of Engineering and Computer Science 14-16 Rue Voltaire, 94270 Le Kremlin-Bicêtre',
-          },
-        });
-
-        const { lat, lng } = response.data;
-        setLocation({ lng, lat });
-      } catch (error) {
-        console.error('Error fetching location:', error);
-      }
-    };
-    fetchLocation();
-  }, []);
-
-  return isLoaded && location ? (
-    <GoogleMap mapContainerStyle={containerStyle} zoom={18} center={{ lat: location.lat, lng: location.lng }}>
-      <MarkerF position={location} />
-      {/* <InfoWindowF position={{ lat: location.lat, lng: location.lng }} zIndex={1}></InfoWindowF> */}
-    </GoogleMap>
-  ) : (
-    <></>
+  return (
+    <div>
+      <Calendar onChange={onChange} className={styles} value={value} />
+    </div>
   );
 }
-
 export default Test2;
