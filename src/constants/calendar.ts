e4041fa2ae6ea2cea37d240fa8716c6b1829f0d7
qooktree1 <qooktree@gmail.com>
2024-02-01 12:30:58 +0900

    Feat: 날짜 및 드랍다운 constant 정리 #47

---
+++ src/constants/calendar.ts
@@ -0,0 +1,3 @@
+export const WEEK = ['SUN', 'MON', 'TUE', 'WED', 'THUR', 'FRI', 'SAT'];
+export const DAYS_IN_SHORT_MONTH = 35;
+export const DAYS_IN_LONG_MONTH = 42;
--- src/constants/dropdown.ts
+++ src/constants/dropdown.ts
@@ -1,2 +1,3 @@
 export const PRICE_LIST = ['낮은 순', '높은 순'] as const;
 export const RESERVE_LIST = ['전체', '예약 신청', '예약 취소', '예약 승인', '예약 거절', '체험 완료'] as const;
+export const CATEGORY_LIST = ['문화 예술', '식음료', '스포츠', '투어', '관광', '웰빙'] as const;
