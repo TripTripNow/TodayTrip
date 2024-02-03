d4edabaec2cf5fa88ce9c8e6a2290c5d8cf500e6
qooktree1 <qooktree@gmail.com>
2024-02-03 20:38:13 +0900

    Fix: 예약현황 페이지 state 수정 #47

--- src/pages/mypage/activities/reservation-dashboard/index.tsx
+++ src/pages/mypage/activities/reservation-dashboard/index.tsx
@@ -1,26 +1,37 @@
-import { ReactElement, useState } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 
+import { INITIAL_DROPDOWN_ITEM } from '@/constants/dropdown';
 import MyPageLayout from '@/components/MyPage/MyPageLayout';
-import Dropdown from '@/components/common/DropDown/Dropdown';
+import Dropdown, { DropdownItems } from '@/components/common/DropDown/Dropdown';
 import Calendar from '@/components/ReservationDashboard/Calendar/Calendar';
-
-import styles from './ReservationDashboard.module.css';
-import { RESERVATION_DETAILS_MONTH_MOCK_DATA, RESERVATION_TITLE_MOCK_DATA } from '@/constants/mock';
+import { RESERVATION_DETAILS_MY_ACTIVITIES_MOCK_DATA } from '@/pages/mypage/activities/reservation-dashboard/mock';
 import NoResult from '@/components/Home/NoResult/NoResult';
+import styles from './ReservationDashboard.module.css';
 
 function ReservationDashboard() {
-  const [dropdownItem, setDropdownItem] = useState<string>(''); // 드랍다운 value 값
-  const experiencesData = RESERVATION_TITLE_MOCK_DATA?.activities.map((activity) => activity.title) ?? [];
+  const [dropDownItem, setDropdownItem] = useState<DropdownItems>(INITIAL_DROPDOWN_ITEM); // 드랍다운 value 값
+  const experiencesData =
+    RESERVATION_DETAILS_MY_ACTIVITIES_MOCK_DATA?.activities.map((activity) => {
+      return {
+        id: activity.id,
+        title: activity.title,
+      };
+    }) ?? [];
 
-  const calendarItems = RESERVATION_DETAILS_MONTH_MOCK_DATA;
+  /** @todo 체험명 변경 시 API 요청 후 받아 온 데이터들을 날짜 데이터에 뿌리기 */
+  const handleExperienceTitle = async () => {};
+
+  useEffect(() => {
+    handleExperienceTitle();
+  }, [dropDownItem]);
 
   return (
     <>
       <h1 className={styles.title}>예약 현황</h1>
       {experiencesData.length > 0 ? (
         <>
-          <Dropdown type="체험" items={experiencesData} setDropdownItem={setDropdownItem} />
-          <Calendar items={calendarItems} />
+          <Dropdown type="체험" items={experiencesData} setDropdownItem={setDropdownItem} dropDownItem={dropDownItem} />
+          <Calendar activityId={dropDownItem.id} />
         </>
       ) : (
         <NoResult text="아직 등록한 체험이 없어요" />
