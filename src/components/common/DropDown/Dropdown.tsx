9a4512a08fde7890c7bbd0ff4765e1fb11fc6fb2
qooktree1 <qooktree@gmail.com>
2024-02-03 20:35:59 +0900

    Fix: 드랍다운 컴포넌트 재사용성을 위한 코드 수정 #47

--- src/components/common/DropDown/Dropdown.tsx
+++ src/components/common/DropDown/Dropdown.tsx
@@ -1,26 +1,29 @@
 import { Dispatch, MouseEvent, SetStateAction, useState } from 'react';
 import clsx from 'clsx';
 
-import { CATEGORY_LIST } from '@/constants/dropdown';
 import ArrowDownIcon from '#/icons/icon-arrowdown.svg';
 import ArrowUpIcon from '#/icons/icon-arrowup.svg';
 import CheckIcon from '#/icons/icon-checkmark.svg';
 import styles from './Dropdown.module.css';
 
-interface DropdownProps {
-  type: '시간' | '카테고리' | '예약한 시간' | '체험';
-  items?: string[];
-  setDropdownItem: Dispatch<SetStateAction<string>>;
+export interface DropdownItems {
+  id: number;
+  title: string;
 }
 
-const dropdownInitialValue = (type: string, items: string[]) => {
-  if (type === '카테고리') return '카테고리';
-  else return items[0];
-};
+interface DropdownProps {
+  type: '시간' | '카테고리' | '예약한 시간' | '체험';
+  items: DropdownItems[];
+  setDropdownItem: Dispatch<SetStateAction<DropdownItems>>;
+  dropDownItem?: DropdownItems;
+  placeholder?: string | null;
+}
 
-function Dropdown({ items, setDropdownItem, type }: DropdownProps) {
+function Dropdown({ items, setDropdownItem, type, dropDownItem, placeholder }: DropdownProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const [value, setValue] = useState(dropdownInitialValue(type, items ?? []));
+  const [value, setValue] = useState(
+    placeholder ? placeholder : dropDownItem?.id === 0 ? items[0].title : dropDownItem?.title,
+  );
 
   const handleDropdownToggle = () => {
     setIsOpen((prev) => !prev);
@@ -30,32 +33,34 @@ function Dropdown({ items, setDropdownItem, type }: DropdownProps) {
     setIsOpen(false);
   };
 
-  const handleDropdownClick = (e: MouseEvent<HTMLDivElement>, val: string) => {
+  const handleDropdownClick = (e: MouseEvent<HTMLDivElement>, val: DropdownItems) => {
     e.preventDefault();
-    setValue(val);
+    setValue(val.title);
     setDropdownItem(val);
     handleDropdownClose();
   };
-
-  const showLists = items ? items.map((item) => item) : CATEGORY_LIST;
 
   return (
     <div className={clsx(styles.container, type === '시간' && styles.timeContainer)} onBlur={handleDropdownClose}>
       {type === '체험' && <p className={styles.subTitle}>체험명</p>}
       <button
         value={value}
-        className={clsx(styles.wrapper, value === '카테고리' && styles.placeholder)}
+        className={clsx(styles.wrapper, value === placeholder && styles.placeholder)}
         onClick={handleDropdownToggle}
       >
         {value}
         {isOpen ? <ArrowUpIcon alt="위쪽 방향 아이콘" /> : <ArrowDownIcon alt="아래쪽 방향 아이콘" />}
       </button>
       {isOpen && (
-        <div className={styles.menu}>
-          {showLists.map((value, index) => (
-            <div key={index} className={styles.list} onMouseDown={(e) => handleDropdownClick(e, value)}>
-              <CheckIcon alt="체크 아이콘" className={styles.icon} />
-              {value}
+        <div className={clsx(styles.menu, type === '시간' && styles.timeMenu)}>
+          {items.map((itemValue) => (
+            <div
+              key={itemValue.id}
+              className={clsx(styles.list, type === '시간' && styles.timeList)}
+              onMouseDown={(e) => handleDropdownClick(e, itemValue)}
+            >
+              {type !== '시간' && <CheckIcon alt="체크 아이콘" className={styles.icon} />}
+              {itemValue.title}
             </div>
           ))}
         </div>
