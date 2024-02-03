b02e0d0352027b328eda5974222e14002962676a
qooktree1 <qooktree@gmail.com>
2024-02-03 20:38:36 +0900

    Feat: 드랍다운 관련 constant 추가 #47

--- src/constants/dropdown.ts
+++ src/constants/dropdown.ts
@@ -1,3 +1,81 @@
 export const PRICE_LIST = ['낮은 순', '높은 순'] as const;
 export const RESERVE_LIST = ['전체', '예약 신청', '예약 취소', '예약 승인', '예약 거절', '체험 완료'] as const;
-export const CATEGORY_LIST = ['문화 예술', '식음료', '스포츠', '투어', '관광', '웰빙'] as const;
+export const CATEGORY_LIST = [
+  {
+    id: 1,
+    title: '문화 예술',
+  },
+  {
+    id: 2,
+    title: '식음료',
+  },
+  {
+    id: 3,
+    title: '스포츠',
+  },
+  {
+    id: 4,
+    title: '투어',
+  },
+  {
+    id: 5,
+    title: '관광',
+  },
+  {
+    id: 6,
+    title: '웰빙',
+  },
+];
+
+export const TIME_LIST = [
+  { id: 1, title: '00:00' },
+  { id: 2, title: '00:30' },
+  { id: 3, title: '01:00' },
+  { id: 4, title: '01:30' },
+  { id: 5, title: '02:00' },
+  { id: 6, title: '02:30' },
+  { id: 7, title: '03:00' },
+  { id: 8, title: '03:30' },
+  { id: 9, title: '04:00' },
+  { id: 10, title: '04:30' },
+  { id: 11, title: '05:00' },
+  { id: 12, title: '05:30' },
+  { id: 13, title: '06:00' },
+  { id: 14, title: '06:30' },
+  { id: 15, title: '07:00' },
+  { id: 16, title: '07:30' },
+  { id: 17, title: '08:00' },
+  { id: 18, title: '08:30' },
+  { id: 19, title: '09:00' },
+  { id: 20, title: '09:30' },
+  { id: 21, title: '10:00' },
+  { id: 22, title: '10:30' },
+  { id: 23, title: '11:00' },
+  { id: 24, title: '11:30' },
+  { id: 25, title: '12:00' },
+  { id: 26, title: '12:30' },
+  { id: 27, title: '13:00' },
+  { id: 28, title: '13:30' },
+  { id: 29, title: '14:00' },
+  { id: 30, title: '14:30' },
+  { id: 31, title: '15:00' },
+  { id: 32, title: '15:30' },
+  { id: 33, title: '16:00' },
+  { id: 34, title: '16:30' },
+  { id: 35, title: '17:00' },
+  { id: 36, title: '17:30' },
+  { id: 37, title: '18:00' },
+  { id: 38, title: '18:30' },
+  { id: 39, title: '19:00' },
+  { id: 40, title: '19:30' },
+  { id: 41, title: '20:00' },
+  { id: 42, title: '20:30' },
+  { id: 43, title: '21:00' },
+  { id: 44, title: '21:30' },
+  { id: 45, title: '22:00' },
+  { id: 46, title: '22:30' },
+  { id: 47, title: '23:00' },
+  { id: 48, title: '23:30' },
+];
+
+export const INITIAL_DROPDOWN_ITEM = { id: 0, title: '' };
