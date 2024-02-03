77c225d0877d6e9e2960f7d637e421fec358f572
youdame <112458620+youdame@users.noreply.github.com>
2024-02-03 15:52:06 +0900

    Feat : 레이아웃 완성 #53

--- src/components/Activities/ReviewList/ReviewList.tsx
+++ src/components/Activities/ReviewList/ReviewList.tsx
@@ -4,6 +4,7 @@ import { reviewData } from '@/components/Activities/mock';
 import StarIcon from '#/icons/icon-star.svg';
 import Image from 'next/image';
 import dayjs from 'dayjs';
+import Pagination from '@/components/common/Pagination/Pagination';
 function ReviewList() {
   const { reviews, totalCount } = reviewData;
 
@@ -20,26 +21,31 @@ function ReviewList() {
           </p>
         </div>
       </div>
-
-      {reviews.map((review) => (
-        <div key={review.id} className={styles.reviewWrapper}>
-          <Image
-            style={{ borderRadius: '100%' }}
-            src={review.user.profileImageUrl}
-            width={45}
-            height={45}
-            alt="프로필 이미지"
-          />
-          <div className={styles.detailReview}>
-            <div className={styles.nameAndDate}>
-              <h3 className={styles.nickname}>{review.user.nickname}</h3>
-              <div className={styles.separator}>|</div>
-              <p className={styles.date}>{dayjs(review.createdAt).format('YYYY.MM.DD')}</p>
+      <div className={styles.reviewListWrapper}>
+        {reviews.map((review, index) => (
+          <>
+            <div key={review.id} className={styles.reviewWrapper}>
+              <Image
+                style={{ borderRadius: '100%' }}
+                src={review.user.profileImageUrl}
+                width={45}
+                height={45}
+                alt="프로필 이미지"
+              />
+              <div className={styles.detailReview}>
+                <div className={styles.nameAndDate}>
+                  <h3 className={styles.nickname}>{review.user.nickname}</h3>
+                  <div className={styles.separator}>|</div>
+                  <p className={styles.date}>{dayjs(review.createdAt).format('YYYY.MM.DD')}</p>
+                </div>
+                <p className={styles.reviewContent}>{review.content}</p>
+              </div>
             </div>
-            <p className={styles.reviewContent}>{review.content}</p>
-          </div>
-        </div>
-      ))}
+            {index !== reviews.length - 1 && <hr className={style.hr} />}
+          </>
+        ))}
+      </div>
+      <Pagination pageNumber={1} totalPages={5} />
     </div>
   );
 }
