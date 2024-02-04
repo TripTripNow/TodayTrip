cf8912d7064d3b6759c14b3aca806dfe0599ac28
박경서 <zerm0907@gmail.com>
2024-02-04 14:19:29 +0900

    Fix: ci 오류 해결 #45

--- src/pages/mypage/activities/reservation-dashboard/index.tsx
+++ src/pages/mypage/activities/reservation-dashboard/index.tsx
@@ -1,20 +1,8 @@
 import { ReactElement, useState } from 'react';
-
 import MyPageLayout from '@/components/MyPage/MyPageLayout';
 
-import styles from './ReservationDashboard.module.css';
-import Dropdown, { ActivityItems } from '@/components/common/DropDown/Dropdown';
-
 function ReservationDashboard() {
-  const [dropdownItem, setDropdownItem] = useState<string | ActivityItems>(''); // 드랍다운 value 값
-
-  return (
-    <div>
-      <h1 className={styles.title}>예약 현황</h1>
-      <Dropdown setDropdownItem={setDropdownItem} type="예약한 시간" />
-      <div></div>
-    </div>
-  );
+  return;
 }
 
 export default ReservationDashboard;
