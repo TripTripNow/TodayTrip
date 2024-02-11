8ec21dc4b59035f9c6a3a337d48bb65f14a223db
qooktree1 <qooktree@gmail.com>
2024-02-12 00:38:47 +0900

    Feat: activity 타입 적용 오류 해결 #70

--- src/types/activities/index.ts
+++ src/types/activities/index.ts
@@ -1,5 +1,3 @@
-import { Activity, Category } from '@/types/common/api';
-
 interface UserInfo {
   id: number;
   nickname: string;
@@ -14,22 +12,6 @@ interface Review {
   rating: number;
   createdAt: string;
   updatedAt: string;
-}
-
-export interface GetActivitiesParam {
-  method: 'offset' | 'cursor';
-  cursorId?: number | null;
-  category?: Category;
-  keyword?: string;
-  sort?: 'most_reviewed' | 'price_asc' | 'price_desc' | 'latest';
-  page?: number;
-  size?: number;
-}
-
-export interface GetActivitiesRes {
-  cursorId: number;
-  totalCount: number;
-  activities: Omit<Activity, 'subImageUrls'>[];
 }
 
 export type GetReviewsRes = {
