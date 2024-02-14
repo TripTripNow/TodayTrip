6a342fb91740e2cac37ac6330714c7b5f7b3bf0a
전수빈 <soobin08011812@gmail.com>
2024-02-14 23:11:46 +0900

    Feat: 결과물 지표 pr 댓글 #69

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -3,10 +3,12 @@ module.exports = {
     collect: {
       startServerCommand: 'npm run dev',
       url: ['http://localhost:3000'],
-      numberOfRuns: 1,
+      numberOfRuns: 5,
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'filesystem',
+      outputDir: './lhci_reports',
+      reportFilenamePattern: '%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%',
     },
   },
 };
