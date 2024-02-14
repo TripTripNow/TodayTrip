4943de523bd5d019231ee58a00d824804fedc0f3
qooktree1 <qooktree@gmail.com>
2024-02-14 12:24:38 +0900

    Fix: 날짜 포매팅 함수 기능 추가 #73

--- src/utils/ReservationDashboard/formatDateStringByDot.ts
+++ src/utils/ReservationDashboard/formatDateStringByDot.ts
@@ -2,6 +2,10 @@ interface FormatDateStringByDotProps {
   year: number;
   month: number;
   day: number;
+  padStart?: boolean;
 }
 
-export const formatDateStringByDot = ({ year, month, day }: FormatDateStringByDotProps) => `${year}.${month}.${day}`;
+export const formatDateStringByDot = ({ year, month, day, padStart }: FormatDateStringByDotProps) => {
+  if (padStart) return `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+  return `${year}-${month}-${day}`;
+};
