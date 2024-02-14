a3ecf6007d203b9104d491243e01340b86475cbc
qooktree1 <qooktree@gmail.com>
2024-02-14 12:25:25 +0900

    Feat: 리액트 쿼리 키 추가 #73

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -1,6 +1,9 @@
 const QUERY_KEYS = {
   usersMe: 'usersMe',
   myActivities: 'myActivities',
+  monthlyReservation: 'monthReservation',
+  dailyReservation: 'dailyReservation',
+  timeReservation: 'timeReservation',
 } as const;
 
 export default QUERY_KEYS;
