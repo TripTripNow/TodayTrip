187cb50268dc085326f842ae923f77d767973206
전수빈 <soobin08011812@gmail.com>
2024-02-15 01:44:07 +0900

    Feat: lighthouse detail add #80

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -5,10 +5,15 @@ module.exports = {
       url: ['http://localhost:3000'],
       numberOfRuns: 5,
     },
-    upload: {
-      target: 'filesystem',
-      outputDir: './lhci_reports',
-      reportFilenamePattern: '%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%',
-    },
+    upload: [
+      {
+        target: 'filesystem',
+        outputDir: './lhci_reports',
+        reportFilenamePattern: '%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%',
+      },
+      {
+        target: 'temporary-public-storage',
+      },
+    ],
   },
 };
