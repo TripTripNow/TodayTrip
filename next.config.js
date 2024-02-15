fac19e762332661fb1e33c7f1e326cf4dc2433b2
전수빈 <soobin08011812@gmail.com>
2024-02-15 20:20:49 +0900

    Refactor: 리뷰 반영 #69

--- next.config.js
+++ next.config.js
@@ -16,7 +16,12 @@ module.exports = {
         port: '',
         pathname: '/globalnomad/activity_registration_image/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/globalnomad/profile_image/**',
+      },
     ],
-    domains: ['i.ibb.co', 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
   },
 };
