22fb2df21374e8cb9bc5b6586215dd1d2984dde5
전수빈 <soobin08011812@gmail.com>
2024-02-15 18:07:33 +0900

    Fix: lighthouse ci #80

--- puppeteer-script.js
+++ puppeteer-script.js
@@ -4,9 +4,11 @@
  */
 module.exports = async (browser, context) => {
   const page = await browser.newPage();
+  console.log('in');
   await page.goto('http://localhost:3000/signin');
-  await page.type('#username', 'admin');
-  await page.type('#password', 'password');
+  await page.type('#email', 'test123@codeit.com');
+  await page.type('#password', 'test12345');
   await page.click('[type="submit"]');
   await page.waitForNavigation();
+  await page.close();
 };
