9aaadb5f7cbed34231488c7349c5274654762bd2
전수빈 <soobin08011812@gmail.com>
2024-02-15 17:33:49 +0900

    Fix: lighthouse ci #80

---
+++ .puppeteer-script.js
@@ -0,0 +1,12 @@
+/**
+ * @param {puppeteer.Browser} browser
+ * @param {{url: string, options: LHCI.CollectCommand.Options}} context
+ */
+module.exports = async (browser, context) => {
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8080/login');
+  await page.type('#username', 'admin');
+  await page.type('#password', 'password');
+  await page.click('[type="submit"]');
+  await page.waitForNavigation();
+};
