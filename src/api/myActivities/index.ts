88691428c9e8a0886b4e261c19cdc437c61cd0a1
박경서 <zerm0907@gmail.com>
2024-02-18 14:16:55 +0900

    Fix: 콘솔 제거 #86

--- src/api/myActivities/index.ts
+++ src/api/myActivities/index.ts
@@ -1,12 +1,6 @@
 import instance from '@/api/axiosInstance';
-import { GetMyActivitiesRes, PatchMyActivityReq } from '@/types/myActivities';
-
-export const patchActivitiesId = async (activityId: number, activityData: PatchMyActivityReq) => {
-  activityId = 62;
-  return await instance.patch(`/my-activities/${activityId}`, activityData);
-};
+import { GetMyActivitiesRes } from '@/types/myActivities';
 
 export const getMyActivities = async (cursorId?: number | undefined): Promise<GetMyActivitiesRes> => {
-  // console.log(cursorId);
   return await instance.get(`/my-activities?size=6${cursorId ? `&cursorId=${cursorId}` : ''}`);
 };
