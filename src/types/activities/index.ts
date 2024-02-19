ea43d291ebd3995976ff8ef0e31ec5f1b4fdc5e7
qooktree1 <qooktree@gmail.com>
2024-02-19 20:00:55 +0900

    Merge #70

--- src/types/activities/index.ts
+++ src/types/activities/index.ts
@@ -1,4 +1,5 @@
 import { Activity, Category } from '@/types/common/api';
+import { ReservationBase } from '@/types/common/api';
 
 interface UserInfo {
   id: number;
@@ -38,3 +39,25 @@ export type GetReviewsRes = {
   reviews: Review[];
   totalCount: number;
 };
+
+export interface GetReviewsParams {
+  activityId: number;
+  page: number;
+  size: 3;
+}
+
+export interface GetAvailableScheduleParams {
+  activityId: number;
+  year: string;
+  month: string;
+}
+
+export interface PostReservationReq {
+  activityId: number;
+  scheduleId: number;
+  headCount: number;
+}
+
+export interface PostReservationRes extends ReservationBase {
+  activityId: number;
+}
