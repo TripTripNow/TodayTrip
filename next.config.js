66cc419a7a3b0ccd289463a981c8850499fd0e96
전수빈 <soobin08011812@gmail.com>
2024-02-22 15:27:31 +0900

    Delete: 임시 코드 삭제 #101

--- next.config.js
+++ next.config.js
@@ -22,12 +22,6 @@ module.exports = {
         port: '',
         pathname: '/globalnomad/profile_image/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
-        port: '',
-        pathname: '/globalnomad/activity_registration_image/**',
-      },
     ],
   },
 };
