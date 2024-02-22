e32922861d7a9117ba53a9b58ec4d3fef36c69ca
Soeun Kim <summerkim0825@gmail.com>
2024-02-22 14:45:57 +0900

    Refactor: 데이트 피커 커스텀 1차 수정 #103

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,5 +1,11 @@
+import DatePickerInput from '@/components/common/DatePicker/DatePicker';
+import { useState } from 'react';
+
 function Test() {
-  return;
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+
+  return <DatePickerInput setIsSelectedDate={Date}/>;
 }
 
 export default Test;
