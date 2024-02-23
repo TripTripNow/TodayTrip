cb39e61e3473ec17d69d344d96b93c7eab684dfa
youdame <112458620+youdame@users.noreply.github.com>
2024-02-23 15:35:41 +0900

    Feat : 태블릿, 모바일일 때도 시간대 없는 날짜 안 보이게 #99

--- src/hooks/common/useBodyScrollLock.ts
+++
@@ -1,8 +0,0 @@
-const useBodyScrollLock = () => {
-  const lockScroll = () => (document.body.style.overflow = 'hidden');
-  const openScroll = () => (document.body.style.overflow = 'unset');
-
-  return { lockScroll, openScroll };
-};
-
-export default useBodyScrollLock;
--- src/types/custom.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.svg' {
-  import type { ReactElement, SVGProps } from 'react';
-
-  const content: (props: SVGProps<SVGElement> & { alt: string }) => ReactElement;
-  export default content;
-}
