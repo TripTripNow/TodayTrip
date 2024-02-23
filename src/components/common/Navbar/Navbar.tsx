bd3a7c0436b847e65c5515e9de6471d3c0d7455e
박경서 <zerm0907@gmail.com>
2024-02-23 16:06:57 +0900

    Fix: 프로필 드롭다운 바닥 여백 조절 #76

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -16,7 +16,7 @@ import useInfiniteScroll from '@/hooks/common/useInfiniteScroll';
 import { Notifications } from '@/types/myNotifications';
 
 function Navbar() {
-  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const [isDropDownOpen, setIsDropDownOpen] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
