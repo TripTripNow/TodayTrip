6da6302cb5e8533809bb2ad3e1be4005bad82e4c
Soeun Kim <summerkim0825@gmail.com>
2024-02-26 13:43:49 +0900

    Refactor: 오타 수정 #124

--- src/components/HeadMeta/HeadMeta.tsx
+++ src/components/HeadMeta/HeadMeta.tsx
@@ -12,7 +12,7 @@ function HeadMeta({ title, description }: HeadMetaProps) {
       <meta name="description" content={description || '새로운 모험을 떠나고 싶다면, TodayTrip과 함께 하세요.'} />
       <meta name="viewport" content="initial-scale=1.0,width=device-width" />
       <meta property="og:title" content={title || 'TodayTrip'} />
-      <meta property="og:type" content={'website'} />
+      <meta property="og:type" content="website" />
       <meta property="g:url" content={'https://today-trip.vercel.app/'} />
       <meta property="og:image" content="#/images/TodayTripLogo2.png" />
       <meta property="og:article:author" content="TripTripNow" />
