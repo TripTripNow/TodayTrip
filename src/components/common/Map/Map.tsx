a061dcd51f86d067773f047c6501dae59c75150f
zermzerm <zerm0907@gmail.com>
2024-02-27 16:56:47 +0900

    Fix: Map 컴포넌트 바깥에 초기화 태그 설정 #142

--- src/components/common/Map/Map.tsx
+++ src/components/common/Map/Map.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { GoogleMap, useJsApiLoader, MarkerF, InfoWindowF } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, MarkerF, InfoWindowF, LoadScript } from '@react-google-maps/api';
 import LocationIcon from '#/icons/icon-location.svg';
 import styles from './Map.module.css';
 
@@ -8,6 +8,29 @@ interface MapProps {
   address: string;
   containerStyle: { width: string; height: string };
 }
+
+class LoadScriptOnlyIfNeeded extends LoadScript {
+  componentDidMount() {
+    const cleaningUp = true; // 청소 모드 플래그, 초기화를 수행하고 있는지 여부를 나타냄
+    const isBrowser = typeof document !== 'undefined'; // 브라우저에서 실행 중인지 확인
+    const isAlreadyLoaded = window.google && window.google.maps && document.querySelector('body.first-hit-completed');
+
+    if (!isAlreadyLoaded && isBrowser) {
+      if (window.google && !cleaningUp) {
+        console.error('google api is already presented');
+        return;
+      }
+
+      this.isCleaningUp().then(this.injectScript); // 초기화가 필요한 경우 스크립트를 주입
+    }
+
+    if (isAlreadyLoaded) {
+      this.setState({ loaded: true }); // 이미 로드된 경우 상태를 설정
+    }
+  }
+}
+
+const libraries = ['places'];
 
 function Map({ address, containerStyle }: MapProps) {
   const { isLoaded } = useJsApiLoader({
@@ -32,27 +55,29 @@ function Map({ address, containerStyle }: MapProps) {
     fetchLocation();
   }, []);
   return (
-    <section className={styles.mapWrapper}>
-      {isLoaded && location && (
-        <GoogleMap mapContainerStyle={containerStyle} zoom={18} center={{ lat: location.lat, lng: location.lng }}>
-          <MarkerF
-            position={location}
-            icon={{ url: '/icons/icon-logoMarker.svg', scaledSize: new window.google.maps.Size(60, 70) }}
-          />
-          <InfoWindowF
-            options={{ pixelOffset: new window.google.maps.Size(0, -70) }}
-            position={{ lat: location.lat, lng: location.lng }}
-            zIndex={1}
-          >
-            <p>{address}</p>
-          </InfoWindowF>
-        </GoogleMap>
-      )}
-      <p className={styles.location}>
-        <LocationIcon alt="지도 마커 아이콘" />
-        {address}
-      </p>
-    </section>
+    <LoadScriptOnlyIfNeeded googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!} libraries={libraries as any}>
+      <section className={styles.mapWrapper}>
+        {isLoaded && location && (
+          <GoogleMap mapContainerStyle={containerStyle} zoom={18} center={{ lat: location.lat, lng: location.lng }}>
+            <MarkerF
+              position={location}
+              icon={{ url: '/icons/icon-logoMarker.svg', scaledSize: new window.google.maps.Size(60, 70) }}
+            />
+            <InfoWindowF
+              options={{ pixelOffset: new window.google.maps.Size(0, -70) }}
+              position={{ lat: location.lat, lng: location.lng }}
+              zIndex={1}
+            >
+              <p>{address}</p>
+            </InfoWindowF>
+          </GoogleMap>
+        )}
+        <p className={styles.location}>
+          <LocationIcon alt="지도 마커 아이콘" />
+          {address}
+        </p>
+      </section>
+    </LoadScriptOnlyIfNeeded>
   );
 }
 
