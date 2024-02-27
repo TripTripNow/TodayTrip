70fecda995799c3944d253f5e4d7121ad6e4da3e
youdame <112458620+youdame@users.noreply.github.com>
2024-02-27 13:41:57 +0900

    Refactor : 리뷰 반영 및 안 쓰는 코드 지우기 #99

--- src/components/common/NoResult/NoResult.tsx
+++ src/components/common/NoResult/NoResult.tsx
@@ -1,5 +1,4 @@
 import NoResultIcon from '#/icons/icon-no-result.svg';
-import clsx from 'clsx';
 import styles from './NoResult.module.css';
 
 interface NoResultProps {
@@ -10,7 +9,7 @@ function NoResult({ text }: NoResultProps) {
   return (
     <div className={styles.defaultContainer}>
       <NoResultIcon alt="검색 결과 없음" />
-      <p className={clsx(styles.default)}>{text}</p>
+      <p className={styles.default}>{text}</p>
     </div>
   );
 }
